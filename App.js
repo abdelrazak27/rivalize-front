2d4155a0ba2d40aff1327fa1da989a40b9906273
abdelrazak27 <113916211+abdelrazak27@users.noreply.github.com>
2024-04-03 14:22:41 +0200

    feat: signup finished fonctionnellement

--- App.js
+++ App.js
@@ -6,10 +6,10 @@ import SignIn from './components/Signin/Signin';
 export default function App() {
   return (
     <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
+      <Text>APP.JS</Text>
       <StatusBar style="auto" />
       <SignUp />
-      <SignIn />
+      {/* <SignIn /> */}
     </View>
   );
 }
--- components/Signup/Signup.jsx
+++ components/Signup/Signup.jsx
@@ -4,24 +4,23 @@ import { getAuth, createUserWithEmailAndPassword } from 'firebase/auth';
 import { app, db } from '../../firebaseConfig';
 import { doc, setDoc } from 'firebase/firestore';
 import DateTimePicker from '@react-native-community/datetimepicker';
-
+import { Picker } from '@react-native-picker/picker';
 
 const auth = getAuth(app);
 
 export default function SignUp() {
-
   const [userDetails, setUserDetails] = useState({
-    email: '',
-    password: '',
-    passwordConfirm: '',
-    firstname: '',
-    lastname: '',
-    birthday: '', 
-    city: '',
-    accountType: '',
-    licenceNumber: '', 
-    playerName: '',
-    playerNumber: '', 
+    email: null,
+    password: null,
+    passwordConfirm: null,
+    firstname: null,
+    lastname: null,
+    birthday: new Date().toISOString().split('T')[0],
+    city: null,
+    accountType: null,
+    licenceNumber: null,
+    playerName: null,
+    playerNumber: null,
   });
 
   const handleChange = (name, value) => {
@@ -31,75 +30,69 @@ export default function SignUp() {
     }));
   };
 
-  const missingField = [
-    'email',
-    'password',
-    'passwordConfirm',
-    'firstname',
-    'lastname',
-    'birthday',
-    'accountType',
-    'licenceNumber',
-    ...(userDetails.accountType === "player" ? ['playerName', 'playerNumber'] : []),
-  ].find(field => !userDetails[field]);
+  const validateFields = () => {
+    const requiredFields = [
+      'email',
+      'password',
+      'passwordConfirm',
+      'firstname',
+      'lastname',
+      'birthday',
+      'accountType',
+      ...(userDetails.accountType === "player" ? ['playerName', 'playerNumber', 'licenceNumber'] : []),
+      ...(userDetails.accountType === "coach" ? ['licenceNumber'] : [])
+    ];
 
-  const handleSignUp = () => {
+    const missingField = requiredFields.find(field => !userDetails[field]);
     if (missingField) {
-      Alert.alert('Erreur', 'Veuillez remplir tous les champs obligatoires.');
-      return;
+      Alert.alert('Erreur', `Veuillez remplir tous les champs obligatoires. Champ manquant: ${missingField}`);
+      return false;
     }
 
     if (userDetails.password !== userDetails.passwordConfirm) {
       Alert.alert('Erreur', 'Les mots de passe ne correspondent pas.');
-      return; 
+      return false;
     }
+
+    return true;
+  };
+
+  const handleSignUp = () => {
+    if (!validateFields()) return;
 
     createUserWithEmailAndPassword(auth, userDetails.email, userDetails.password)
       .then(userCredentials => {
         const user = userCredentials.user;
-        console.log('*****');
-        console.log('Un nouveau compte vient d\'être créé');
-        console.log('UID   : ', user.uid);
-        console.log('Email : ', user.email);
-        console.log('*****');
+
+        const userData = {
+          ...userDetails,
+          birthday: userDetails.birthday || new Date().toISOString().split('T')[0],
+        };
+        delete userData.password;
+        delete userData.passwordConfirm;
 
         const userRef = doc(db, 'utilisateurs', user.uid);
-        setDoc(userRef, {
-          firstname: userDetails.firstname,
-          lastname: userDetails.lastname,
-          birthday: userDetails.birthday,
-          city: userDetails.city,
-          account_type: userDetails.accountType,
-          licence_number: userDetails.licenceNumber,
-          player_name: userDetails.playerName,
-          player_number: userDetails.playerNumber
-          
-        })
-        .then(() => {
-          console.log('Utilisateur ajouté à Firestore avec succès');
-        })
-        .catch((error) => {
-          console.error('Erreur lors de l\'ajout de l\'utilisateur à Firestore: ', error);
-        });
+        setDoc(userRef, userData)
+          .then(() => {
+            console.log('Utilisateur ajouté à Firestore avec succès');
+          })
+          .catch((error) => {
+            console.error('Erreur lors de l\'ajout de l\'utilisateur à Firestore: ', error);
+          });
 
       })
       .catch((error) => {
-        const errorCode = error.code;
-        const errorMessage = getErrorMessage(errorCode);
+        const errorMessage = getErrorMessage(error.code);
         Alert.alert('Erreur d\'inscription', errorMessage);
       });
   };
 
   const getErrorMessage = (errorCode) => {
     switch (errorCode) {
-      case 'auth/email-already-in-use':
-        return 'Cette adresse email est déjà utilisée par un autre compte.';
-      case 'auth/weak-password':
-        return 'Le mot de passe doit contenir au moins 6 caractères.';
-      case 'auth/invalid-email':
-        return 'L\'adresse email saisie n\'est pas valide.';
-      default:
-        return 'Une erreur inattendue est survenue. Veuillez réessayer plus tard.';
+      case 'auth/email-already-in-use': return 'Cette adresse email est déjà utilisée par un autre compte.';
+      case 'auth/weak-password': return 'Le mot de passe doit contenir au moins 6 caractères.';
+      case 'auth/invalid-email': return 'L\'adresse email saisie n\'est pas valide.';
+      default: return 'Une erreur inattendue est survenue. Veuillez réessayer plus tard.';
     }
   };
 
@@ -107,17 +100,20 @@ export default function SignUp() {
     <View>
       <Text>STEP 1 : information de connexion</Text>
       <TextInput
+        testID='email'
         placeholder="Email *"
         value={userDetails.email}
         onChangeText={(text) => handleChange('email', text)}
       />
       <TextInput
+        testID='password'
         placeholder="Mot de passe *"
         value={userDetails.password}
         onChangeText={(text) => handleChange('password', text)}
         secureTextEntry
       />
       <TextInput
+        testID='passwordConfirm'
         placeholder="Confirmer votre mot de passe *"
         value={userDetails.passwordConfirm}
         onChangeText={(text) => handleChange('passwordConfirm', text)}
@@ -126,18 +122,19 @@ export default function SignUp() {
       <Button title='Suivant' />
       <Text>STEP 2 : informations personnelles</Text>
       <TextInput
+        testID='firstname'
         placeholder="Prénom *"
         value={userDetails.firstname}
         onChangeText={(text) => handleChange('firstname', text)}
       />
       <TextInput
+        testID='lastname'
         placeholder="Nom *"
         value={userDetails.lastname}
         onChangeText={(text) => handleChange('lastname', text)}
       />
-
       <DateTimePicker
-        testID="dateTimePicker"
+        testID='birthday'
         value={userDetails.birthday ? new Date(userDetails.birthday) : new Date()}
         mode="date"
         display="default"
@@ -149,10 +146,8 @@ export default function SignUp() {
           }));
         }}
       />
-
-
-
       <TextInput
+        testID='city'
         placeholder="Ville"
         value={userDetails.city}
         onChangeText={(text) => handleChange('city', text)}
@@ -160,27 +155,45 @@ export default function SignUp() {
       <Button title='Précédent' />
       <Button title='Suivant' />
       <Text>STEP 3 : informations professionnels</Text>
-      <TextInput
-        placeholder="Type de compte *"
-        value={userDetails.accountType}
-        onChangeText={(text) => handleChange('accountType', text)}
-      />
-      <TextInput
-        placeholder="Numéro de licence *"
-        value={userDetails.licenceNumber}
-        onChangeText={(text) => handleChange('licenceNumber', text)}
-      />
-      <TextInput
-        placeholder="Nom de joueur *"
-        value={userDetails.playerName}
-        onChangeText={(text) => handleChange('playerName', text)}
-      />
-      <TextInput
-        placeholder="Numéro de joueur *"
-        value={userDetails.playerNumber}
-        onChangeText={(text) => handleChange('playerNumber', text)}
-      />
-      <Button title="S'inscrire" onPress={handleSignUp} />
+      <Picker
+        testID='accountType'
+        selectedValue={userDetails.accountType}
+        onValueChange={itemValue =>
+          setUserDetails(prevDetails => ({
+            ...prevDetails,
+            accountType: itemValue
+          }))
+        }>
+        <Picker.Item label="Choisir" />
+        <Picker.Item label="Joueur" value="player" />
+        <Picker.Item label="Entraîneur" value="coach" />
+        <Picker.Item label="Visiteur" value="visitor" />
+      </Picker>
+      {(userDetails.accountType === "player" || userDetails.accountType === "coach") && (
+        <TextInput
+          testID='licenceNumber'
+          placeholder="Numéro de licence *"
+          value={userDetails.licenceNumber}
+          onChangeText={(text) => handleChange('licenceNumber', text)}
+        />
+      )}
+      {userDetails.accountType === "player" && (
+        <>
+          <TextInput
+            testID='playerName'
+            placeholder="Nom de joueur *"
+            value={userDetails.playerName}
+            onChangeText={(text) => handleChange('playerName', text)}
+          />
+          <TextInput
+            testID='playerNumber'
+            placeholder="Numéro de joueur *"
+            value={userDetails.playerNumber}
+            onChangeText={(text) => handleChange('playerNumber', text)}
+          />
+        </>
+      )}
+      <Button testID='signupButton' title="S'inscrire" onPress={handleSignUp} />
     </View>
   );
 }
