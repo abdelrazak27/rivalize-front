b42f3414583a438028676ad3aa334980a88b657c
Abdelrazak AMMARA <abdelrazak.ammara@mail-ecv.fr>
2024-05-28 11:48:27 +0200

    chat tournament style ok finished

--- screens/ChatScreen.jsx
+++ screens/ChatScreen.jsx
@@ -1,9 +1,12 @@
 import React, { useState, useEffect } from 'react';
-import { GiftedChat } from 'react-native-gifted-chat';
+import { GiftedChat, InputToolbar, Bubble, Send, Composer } from 'react-native-gifted-chat';
 import { collection, query, onSnapshot, addDoc, serverTimestamp } from "firebase/firestore";
 import { db } from '../firebaseConfig';
 import { View, Text, ActivityIndicator, StyleSheet, LogBox } from 'react-native';
 import { useUser } from '../context/UserContext';
+import colors from '../styles/colors';
+import { fonts } from '../styles/fonts';
+import FontAwesome from 'react-native-vector-icons/FontAwesome';
 
 function ChatScreen({ route }) {
     const { tournamentId } = route.params;
@@ -53,6 +56,62 @@ function ChatScreen({ route }) {
         await Promise.all(writes);
     };
 
+    const renderInputToolbar = (props) => {
+        return (
+            <InputToolbar
+                {...props}
+                primaryStyle={{ alignItems: 'center' }}
+            />
+        );
+    };
+
+    const renderBubble = (props) => {
+        return (
+            <Bubble
+                {...props}
+                wrapperStyle={{
+                    left: {
+                        backgroundColor: colors.lightgrey,
+                    },
+                    right: {
+                        backgroundColor: colors.primary,
+                    },
+                }}
+                textStyle={{
+                    left: {
+                        color: colors.darkgrey,
+                        fontFamily: fonts.OutfitRegular,
+                        padding: 5,
+                    },
+                    right: {
+                        color: "white",
+                        fontFamily: fonts.OutfitRegular,
+                        padding: 5,
+                    },
+                }}
+            />
+        );
+    };
+
+    const renderSend = (props) => {
+        return (
+            <Send {...props}>
+                <View style={styles.sendingContainer}>
+                    <FontAwesome name="send" size={24} color={colors.primary} />
+                </View>
+            </Send>
+        );
+    };
+
+    const renderComposer = (props) => {
+        return (
+            <Composer
+                {...props}
+                textInputStyle={styles.composer}
+            />
+        );
+    };
+
     if (!user) {
         return (
             <View style={styles.loadingContainer}>
@@ -64,15 +123,21 @@ function ChatScreen({ route }) {
 
     return (
         <View style={styles.container}>
-            <GiftedChat
-                messages={messages}
-                onSend={messages => onSend(messages)}
-                user={{
-                    _id: user.uid,
-                    name: `${user.firstname} ${user.lastname}`
-                }}
-                renderUsernameOnMessage={true}
-            />
+            <View style={styles.chatContainer}>
+                <GiftedChat
+                    messages={messages}
+                    onSend={messages => onSend(messages)}
+                    user={{
+                        _id: user.uid,
+                        name: `${user.firstname} ${user.lastname}`
+                    }}
+                    renderUsernameOnMessage={true}
+                    renderInputToolbar={renderInputToolbar}
+                    renderBubble={renderBubble}
+                    renderSend={renderSend}
+                    renderComposer={renderComposer}
+                />
+            </View>
         </View>
     );
 }
@@ -81,10 +146,20 @@ const styles = StyleSheet.create({
     container: {
         flex: 1,
     },
+    chatContainer: {
+        flex: 1,
+        paddingHorizontal: 30,
+    },
     loadingContainer: {
         flex: 1,
         justifyContent: 'center',
         alignItems: 'center',
+    },
+    sendingContainer: {
+        justifyContent: 'center',
+        alignItems: 'center',
+        height: 44,
+        width: 44,
     },
 });
 
